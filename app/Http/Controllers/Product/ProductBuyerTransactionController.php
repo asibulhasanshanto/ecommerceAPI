227ed307b1e759dcb2017d8bee93c1094d374e1a
Asibul <asibulhasan.rucse.18@gmail.com>
2020-09-01 11:41:31 +0600

    complex operation on product with transaction and an event for handling availability is completed

---
+++ app/Http/Controllers/Product/ProductBuyerTransactionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Buyer;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Product;
+use App\Seller;
+use App\Transaction;
+use App\User;
+use Facade\FlareClient\Api;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ProductBuyerTransactionController extends ApiController
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,Product $product, User $buyer)
+    {
+        $rules = [
+            'quantity'=> 'required|integer|min:1'
+        ];
+
+        $this->validate($request,$rules);
+        if(!$buyer->id == $product->seller_id)
+        {
+            return $this->errorResponse('the buyer must be different from seller',409);
+        }
+        if(!$buyer->isVerified())
+        {
+            return $this->errorResponse('the buyer must be a verified user',409);
+        }
+        if(!$product->seller->isVerified())
+        {
+            return $this->errorResponse('the seller must be a verified user',409);
+        }
+        if(! $product->isAvailable())
+        {
+            return $this->errorResponse('the product is not available',409);
+        }
+        if($product->quantity < $request->quantity)
+        {
+            return $this->errorResponse('this product does not have enough units for this transaction',409);
+        }
+        return DB::transaction(function () use($request,$product,$buyer){
+            $product->quantity -=$request->quantity;
+            $product->save();
+
+            $transaction = Transaction::create([
+                'quantity'=> $request->quantity,
+                'buyer_id' =>$buyer->id,
+                'product_id'=>$product->id,
+            ]);
+            return $this->showOne($transaction,201);
+        });
+    }
+
+}
---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Category;
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+
+     //this methode will add a category with this product
+    public function update(Request $request, Product $product, Category $category)
+    {
+        /* we can use attach,sync,and syncWithoutDetaching method to interact with many to many
+        relationship
+        1. attach=>can attache duplicate values
+        2.sync=> deletes all the values and adds the current value
+        3. syncWithoutDetaching=> doesn't attach duplicate value and also doesn't delete any of them */
+
+        $product->categories()->syncWithoutDetaching([$category->id]);
+        return $this->showAll($product->categories);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product,Category $category)
+    {
+        if(!$product->categories()->find($category->id))
+        {
+            return $this->errorResponse('the specified category is not a category of that product',404);
+        }
+        $product->categories()->detach($category->id);
+        return $this->showAll($product->categories);
+    }
+}
