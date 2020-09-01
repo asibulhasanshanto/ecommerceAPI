8372c7b9c4ec9836e6d4b192535839a7127b450e
Asibul <asibulhasan.rucse.18@gmail.com>
2020-09-01 12:09:07 +0600

    image upload for a product is completed

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -8,6 +8,7 @@ use App\Product;
 use App\Seller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
@@ -45,7 +46,7 @@ class SellerProductController extends ApiController
         $data = $request->all();
 
         $data['status'] =Product::UNAVAILABLE_PRODUCT;
-        $data['image'] = '1.jpg';
+        $data['image'] = $request->image->store('');
         $data['seller_id'] = $seller->id;
 
         $product = Product::create($data);
@@ -89,6 +90,12 @@ class SellerProductController extends ApiController
                 return $this->errorResponse('An active product must have at least one category',409);
             }
         }
+        if($request->hasFile('image'))
+        {
+            Storage::delete($product->image);
+
+            $product->image = $request->image->store();
+        }
         //checking if anything is updated or not
         if($product->isClean())
         {
@@ -108,6 +115,7 @@ class SellerProductController extends ApiController
     {
         $this->checkSeller($seller,$product);
         $product->delete();
+        Storage::delete($product->image);
 
         return $this->showOne($product);
     }
