d658aa946e7f80a46109dd54c9e9fce2da6cd00b
Asibul <asibulhasan.rucse.18@gmail.com>
2020-09-24 23:42:33 +0600

    obtained access token using client credentials and used it

--- app/User.php
+++ app/User.php
@@ -9,10 +9,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, SoftDeletes, HasApiTokens;
 
     const VERIFIED_USER = '1';
     const UNERIFIED_USER = '0';
--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Products
 */
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
 Route::resource('products.buyers', 'Product\ProductBuyerController', ['only' => ['index']]);
-Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index','update','destroy']]);
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
 Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
 Route::resource('products.buyers.transactions', 'Product\ProductBuyerTransactionController', ['only' => ['store']]);
 
@@ -48,7 +48,7 @@ Sellers
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
 Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
-Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create','show','edit']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
 /*
 Transaction
@@ -67,4 +67,4 @@ Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]
 Route::get('users/verify/{token}', 'User\UserController@verify')->name('verify');
 Route::get('users/{user}/resend', 'User\UserController@resend')->name('resend');
 
-
+Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
